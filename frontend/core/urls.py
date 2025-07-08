f3dbcc5f98a04272ffee337fd650b26415fe9aed
Irymar <irymar97@gmail.com>
2025-07-08 16:10:30 +0300

    Test

--- frontend/core/urls.py
+++ frontend/core/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('test_rds/', views.test_rds, name='test_rds2'),
-    path('test_redis/', views.test_redis, name='test_redis2'),
+    path('test_rds/', views.test_rds, name='test_rds'),
+    path('test_redis/', views.test_redis, name='test_redis'),
 ]
